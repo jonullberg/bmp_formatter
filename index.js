b03da139bc7a1d3d9b283babeec622597feb8005
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-05-03 15:08:42 -0700

    added logic to choose between palette or non-palette bmp

--- index.js
+++ index.js
@@ -3,26 +3,34 @@
 var fs = require('fs');
 var change = require('./lib/change');
 var create = require('./lib/create');
+var newFile = process.argv[2];
+var color = process.argv[3];
+console.log(newFile);
 
-fs.readFile('./tmp/test.bmp', function(err, data) {
+fs.readFile('./tmp/non-palette-bitmap.bmp', function(err, data) {
 	if (err) console.log(err);
 	var bitmap = create.createFileHeader(data);
 	bitmap = create.createInfoHeader(bitmap, data);
-	bitmap = create.createColorData(bitmap, data);
-	console.log(bitmap);
-
+	if(bitmap.infoHeader.biteClrUsed !== 0) {
+		bitmap = create.createColorData(bitmap, data);
+	} else {
+		bitmap = create.createPixelData(bitmap, data);
+	}
 	if(bitmap.infoHeader.biteSize === (12 || 64)) {
 		var i = 78;
 	} else {
-		var i = 54;
+		i = 54;
 	}
 	var newBuffer = new Buffer(data);
-	var color = "r";
-	change.changeValue(newBuffer, bitmap.colorTable, color, 255, i);
-	// // var start = bitmap.fileHeader.bfOffBits;
+	// var color = "r";
+	if(bitmap.infoHeader.biteClrUsed !== 0) {
+		change.changePaletteValue(newBuffer, bitmap.colorTable, color, 255, i);
+	} else {
+		change.changePixelValue(newBuffer, bitmap.pixelData, color, 255, 54);
+	}
 
-	// fs.writeFile('./tmp/new.bmp', newBuffer, function(err) {
-	// 	console.log('It\'s Saved!!!')
-	// });
+	fs.writeFile('./tmp/new1.bmp', newBuffer, function(err) {
+		console.log('It\'s Saved!!!');
+	});
 });
 
