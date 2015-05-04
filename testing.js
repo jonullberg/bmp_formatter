0290c9d0ccd2e04bc3c4fe81580fc0fb86ed0f31
tcandens <joey.thies@gmail.com>
2015-05-03 20:15:55 -0700

    Tests for change.js

---
+++ testing.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var fs = require('fs');
+var create = require('./lib/create');
+var change = require('./lib/change');
+var path = './tmp/tester.bmp';
+
+fs.readFile(path, function(err, data) {
+	if (err) console.log(err);
+	var i = 0;
+	var bitmap = {};
+	var startPoint = 0;
+	var newBuffer = new Buffer(data);
+	// switch(color) {
+	// 	case 'b':
+	// 	case 'blue':
+	// 		startPoint = 54;
+	// 		break;
+	// 	case 'g':
+	// 	case 'green':
+	// 		startPoint = 55;
+	// 		break;
+	// 	case 'r':
+	// 	case 'red':
+	// 		startPoint = 56;
+	// 		break;
+	// 	default:
+	// 		console.log('You did not enter a valid color choice, \n Please choose either r(ed), g(reen), or b(lue)');
+	// 		break;
+	// }
+  startPoint = 56;
+  var color = 'red';
+	bitmap = create.createFileHeader(data);
+	bitmap = create.createInfoHeader(bitmap, data);
+	// if(bitmap.infoHeader.biteBitCount <= 8) {
+	// bitmap = create.createColorData(bitmap, data);
+	// change.changePaletteValue(newBuffer, bitmap.colorTable, color, 255, startPoint);
+	// } else {
+	bitmap = create.createPixelData(bitmap, data);
+	change.changePixelValue(newBuffer, bitmap.pixelData, color, 255, startPoint);
+	// }
+
+  console.dir(newBuffer.toString('hex'));
+  // console.log(bitmap.colorTable);
+	// fs.writeFile('./tmp/new1.bmp', newBuffer, function(err) {
+	// 	if(err) console.log(err);
+	// });
+});
