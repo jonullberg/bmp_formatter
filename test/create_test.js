5d7b6f6c5fa57ca574d6a83a5fe0f311aacb6a3e
tcandens <joey.thies@gmail.com>
2015-05-04 22:47:56 -0700

    Working create.js tests

--- test/create_test.js
+++ test/create_test.js
@@ -20,15 +20,30 @@ describe('Create.js', function() {
 
   describe('@function createColorData()', function() {
     it('Should create color palette data within array', function(done) {
-      fs.readFile('./tmp/tester.bmp', function(err, data) {
+      fs.readFile('./tmp/bitmap1.bmp', function(err, data) {
         if (err) console.log(err);
         var bitmap = {};
-        create.createHeader(data, bitmap, function(data, bmp) {
-          create.createColorData(data, bitmap);
+        create.createHeader(data, bitmap, function(bitmap, data) {
+          bitmap = create.createColorData(bitmap, data);
           expect(bitmap.colorTable[0].r).to.be.within(0, 255);
           done();
         });
       });
     });
   });
+
+  describe('@function createPixelData()', function() {
+    it('Should create pixel color data in array', function(done) {
+      fs.readFile('./tmp/non-palette-bitmap.bmp', function(err, data) {
+        if (err) console.log(err);
+        var bitmap = {};
+        create.createHeader(data, bitmap, function(bitmap, data) {
+          create.createPixelData(bitmap, data);
+          expect(bitmap.pixelData[0].r).to.be.within(0, 255);
+          done();
+        });
+      });
+    });
+  });
+
 });
